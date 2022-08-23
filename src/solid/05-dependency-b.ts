228d17788a5843fa45d70ae53ad9b5de75d87d1a
natogourmet <natogourmet@hotmail.com>
2022-08-22 20:02:12 -0500

    dependency-injection but no dependency-inversion

---
+++ src/solid/05-dependency-b.ts
@@ -0,0 +1,26 @@
+import { LocalDataBaseService, JsonDataBaseService } from './05-dependency-c';
+
+export interface Post {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+export class PostService {
+  private posts: Post[] = [];
+
+  constructor( private postProvider: JsonDataBaseService ) {}
+
+  async getPosts() {
+    // const jsonDB = new LocalDataBaseService();
+    // this.posts = await jsonDB.getFakePosts();
+
+    // const jsonDb = new JsonDataBaseService();
+    // this.posts = await jsonDb.getPost();
+
+    this.posts = await this.postProvider.getPost();
+
+    return this.posts;
+  }
+}
